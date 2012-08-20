31ce48d31d3e38e23467279ba88731534c03b0c5
PPilger <peter@pilgerstorfer.at>
2012-08-20 13:21:30 +0200

    changed Parameters for portolan atlas

--- src/main/java/TextDetection.java
+++ src/main/java/TextDetection.java
@@ -7,39 +7,41 @@ public class TextDetection {
 	 * @throws InterruptedException
 	 */
 	public static void main(String[] args) throws InterruptedException {
-		Image img = new Image("samples" + File.separator + "PORTOLAN_ATLAS.jpg");
+		Image img = new Image("samples" + File.separator + "PORTOLAN_ATLAS_S.jpg");
 		img.addProcessor(new ColorToGrayProcessor());
 		img.addProcessor(new ThresholdProcessor(165)); // 170 TB, 200 BI, 150 PA
 		img.addProcessor(new InvertProcessor());
 		
-		//img.addProcessor(new ColorEraseProcessor(70, 130, 70, 110, 80, 100, 10, true));
-		//img.addProcessor(new ColorEraseProcessor(70, 110, 70, 100, 80, 90, 10, true));
+		//img.addProcessor(new RGRatioDisplayProcessor("1", 1200, 800, 0.5));
+		//img.addProcessor(new RBRatioDisplayProcessor("1", 1200, 800, 0.5));
+		//img.addProcessor(new GBRatioDisplayProcessor("1", 1200, 800, 0.5));
 		
-		img.addProcessor(new RGRatioDisplayProcessor("1", 1200, 800, 0.5));
-		img.addProcessor(new RBRatioDisplayProcessor("1", 1200, 800, 0.5));
-		img.addProcessor(new GBRatioDisplayProcessor("1", 1200, 800, 0.5));
-		
+		//img.addProcessor(new RGBRatioEraseProcessor(0.9, 3, 1, 3, 1, 3, 2, true));
+		//img.addProcessor(new ImageDisplayProcessor("1", 1200, 800));
+		//img.addProcessor(new RGBRatioEraseProcessor(0.9, 3, 0.8, 3, 0.95, 3, 2, true));
+		//img.addProcessor(new ImageDisplayProcessor("3", 1200, 800));
 
-		img.addProcessor(new RGBRatioEraseProcessor(0.9, 3, 1, 3, 1, 3, 2, true));
-		//img.addProcessor(new ImageDisplayProcessor("3"));
-		
-
-		img.addProcessor(new RGBRatioEraseProcessor(0.9, 2, 0, 1000, 0, 1000, 2, true));
-		img.addProcessor(new TempDisplayProcessor("3", 1200, 800));
-		img.addProcessor(new RGBRatioEraseProcessor(0.9, 2, 0, 1000, 0, 1000, 2, true));
-		img.addProcessor(new TempDisplayProcessor("3", 1200, 800));
-		img.addProcessor(new RGBRatioEraseProcessor(0.9, 2, 0, 1000, 0, 1000, 2, true));
-		img.addProcessor(new TempDisplayProcessor("3", 1200, 800));
-		img.addProcessor(new RGBRatioEraseProcessor(0.9, 2, 0, 1000, 0, 1000, 2, true));
-		img.addProcessor(new TempDisplayProcessor("3", 1200, 800));
-		
-		img.addProcessor(new RGBRatioEraseProcessor(0, 1000, 0, 1000, 0, 1000, 2, true));
-		
-		//img.addProcessor(new ColorEraseProcessor(0, 50, 0, 50, 0, 100, 10));
-		//img.addProcessor(new ColorEraseProcessor(0, 50, 0, 50, 0, 100, 10));
+		int i = 10;
+		/*int dil = 1;
+		img.addProcessor(new RGBRatioEraseProcessor(0, 3, 0, 3, 0.95, 1, dil, true));
+		img.addProcessor(new ImageDisplayProcessor(""+i++, 1200, 800));
+		img.addProcessor(new RGBRatioEraseProcessor(0, 3, 0, 3, 0.95, 1.1, dil, true));
+		img.addProcessor(new ImageDisplayProcessor(""+i++, 1200, 800));
+		img.addProcessor(new RGBRatioEraseProcessor(0, 3, 0, 3, 0.95, 1.2, dil, true));
+		img.addProcessor(new ImageDisplayProcessor(""+i++, 1200, 800));
+		img.addProcessor(new RGBRatioEraseProcessor(0, 3, 0, 3, 0.95, 1.3, dil, true));
+		img.addProcessor(new ImageDisplayProcessor(""+i++, 1200, 800));
+		img.addProcessor(new RGBRatioEraseProcessor(0, 3, 0, 3, 0.95, 1.4, dil, true));
+		img.addProcessor(new ImageDisplayProcessor(""+i++, 1200, 800));
+		img.addProcessor(new RGBRatioEraseProcessor(0, 3, 0, 3, 0.95, 3, dil, true));
+		img.addProcessor(new ImageDisplayProcessor("a"+i++, 1200, 800));
+		*/
 		
 		
-		img.addProcessor(new ThicknessProcessor(1, 5));
+		new ImageDisplay("", 1200,800).show(img.iplImage());
+		img.addProcessor(new ImageDisplayProcessor("a"+i++, 1200, 800));
+		img.addProcessor(new ThicknessProcessor(1, 7));
+		img.addProcessor(new ImageDisplayProcessor("a"+i++, 1200, 800));
 		//img.addProcessor(new RemoveLinesProcessor(60));
 		img.addProcessor(new DilateProcessor(3));
 		img.addProcessor(new CloseProcessor(3));
